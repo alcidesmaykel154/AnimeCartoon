4ed80ca12c84b2d8d33208dc608738527d73a4d1
alcidesmaykel154 <alcidesmaykel154@gmail.com>
2026-04-19 15:31:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-//npm run devStart
 const express = require("express")
 const app = express()
-
-//const { addonBuilder, serveHTTP, publishToCentral } = require('stremio-addon-sdk')
+const fsPromises = require("fs/promises")
 
 function setCORS(_req, res, next) {
   res.header(`Access-Control-Allow-Origin`, `*`);
@@ -13,187 +11,24 @@ function setCORS(_req, res, next) {
 app.use(setCORS);
 
 app.use(express.static('public'))
-app.set('view engine', 'ejs');
 
-const fsPromises = require("fs/promises")
 function ReadManifest() {
   return fsPromises.readFile('./package.json', 'utf8').then((data) => {
     const packageJSON = JSON.parse(data);
 
     let manifest = {
-      "id": 'com.' + packageJSON.name.replaceAll('-', '.'),
+      "id": "com.lacartoons.stremio",
       "version": packageJSON.version,
-      "name": "AnimeFLV, AnimeAV1, Henaojara, TioAnime & LACartoons",
-      "logo": "https://play-lh.googleusercontent.com/ZIjIwO5FJe9R1rplSd4uz54OwBxQhwDcznjljSPl2MgHaCoyF3qG6R4kRMCB40f4l2A=w256",
+      "name": "LACartoons",
+      "logo": "https://www.lacartoons.com/assets/favicon-da907fb005ebb8eef5d862002ec98c648a927a4fce446874a672b6daee984fb9.ico",
       "background": "https://images6.alphacoders.com/113/1135890.jpg",
       "description": packageJSON.description,
       "catalogs": [
         {
-          id: "animeflv", type: "AnimeFLV", name: "search results",
-          extra: [{ name: "search", isRequired: true },
-          {
-            name: "genre",
-            options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia",
-              "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia",
-              "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio",
-              "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai",
-              "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros",
-              "yaoi", "yuri"],
-            optionsLimit: 1, isRequired: false
-          },
-          { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "animeav1", type: "AnimeAV1", name: "search results",
-          extra: [{ name: "search", isRequired: true },
-          {
-            name: "genre",
-            options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia",
-              "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia",
-              "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio",
-              "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai",
-              "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros",
-              "yaoi", "yuri"],
-            optionsLimit: 1, isRequired: false
-          },
-          { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "henaojara", type: "Henaojara", name: "search results",
-          extra: [{ name: "search", isRequired: true },
-          {
-            name: "genre",
-            options: ["accion", "aenime", "anime-latino", "artes-marciales", "aventura", "aventuras", "blu-ray",
-              "carreras", "castellano", "ciencia-ficcion", "comedia", "comida", "cyberpunk", "demencia", "dementia",
-              "demonios", "deportes", "drama", "ecchi", "escolares", "escuela", "espacial", "fantasia", "gore",
-              "harem", "historia-paralela", "historico", "horror", "infantil", "josei", "juegos", "latino", "lucha",
-              "magia", "mecha", "militar", "misterio", "monogatari", "musica", "parodia", "parodias", "policia",
-              "psicologico", "recuentos-de-la-vida", "recuerdos-de-la-vida", "romance", "samurai", "seinen", "shojo",
-              "shonen", "shoujo", "shounen", "shounen-ai", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros", "yaoi", "yuri"],
-            optionsLimit: 1, isRequired: false
-          },
-          { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "tioanime", type: "TioAnime", name: "search results",
-          extra: [{ name: "search", isRequired: true },
-            {
-              name: "genre",
-              options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia",
-                "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia",
-                "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio",
-                "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai",
-                "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros",
-                "yaoi", "yuri"],
-                optionsLimit: 1, isRequired: false
-            },
-            { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "lacartoons", type: "LACartoons", name: "search results",
-          extra: [{ name: "search", isRequired: true }]
-        },
-        {
-          id: "lacartoons|all", type: "LACartoons", name: "LACartoons"
-        },
-        {
-          id: "animeflv|genres", type: "AnimeFLV", name: "AnimeFLV",
-          extra: [
-            {
-              name: "genre",
-              options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia",
-                "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia",
-                "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio",
-                "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai",
-                "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros",
-                "yaoi", "yuri"],
-              optionsLimit: 1, isRequired: true
-            },
-            { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "animeav1|genres", type: "AnimeAV1", name: "AnimeAV1",
-          extra: [
-            {
-              name: "genre",
-              options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia",
-                "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia",
-                "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio",
-                "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai",
-                "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros",
-                "yaoi", "yuri"],
-              optionsLimit: 1, isRequired: true
-            },
-            { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "henaojara|genres", type: "Henaojara", name: "Henaojara",
-          extra: [
-            {
-              name: "genre",
-              options: ["accion", "aenime", "anime-latino", "artes-marciales", "aventura", "aventuras", "blu-ray",
-              "carreras", "castellano", "ciencia-ficcion", "comedia", "comida", "cyberpunk", "demencia", "dementia",
-              "demonios", "deportes", "drama", "ecchi", "escolares", "escuela", "espacial", "fantasia", "gore",
-              "harem", "historia-paralela", "historico", "horror", "infantil", "josei", "juegos", "latino", "lucha",
-              "magia", "mecha", "militar", "misterio", "monogatari", "musica", "parodia", "parodias", "policia",
-              "psicologico", "recuentos-de-la-vida", "recuerdos-de-la-vida", "romance", "samurai", "seinen", "shojo",
-              "shonen", "shoujo", "shounen", "shounen-ai", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros", "yaoi", "yuri"],
-              optionsLimit: 1, isRequired: true
-            },
-            { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "tioanime|genres", type: "TioAnime", name: "TioAnime",
-          extra: [
-            {
-              name: "genre",
-              options: ["accion", "artes-marciales", "aventura", "carreras", "ciencia-ficcion", "comedia",
-                "demencia", "demonios", "deportes", "drama", "ecchi", "escolares", "espacial", "fantasia",
-                "harem", "historico", "infantil", "josei", "juegos", "magia", "mecha", "militar", "misterio",
-                "musica", "parodia", "policia", "psicologico", "recuentos-de-la-vida", "romance", "samurai",
-                "seinen", "shoujo", "shounen", "sobrenatural", "superpoderes", "suspenso", "terror", "vampiros",
-                "yaoi", "yuri"],
-                optionsLimit: 1, isRequired: true
-            },
-            { name: "skip", isRequired: false }
-          ]
-        },
-        {
-          id: "animeflv|onair", type: "AnimeFLV", name: "On Air"
-        },
-        {
-          id: "animeav1|onair", type: "AnimeAV1", name: "On Air"
-        },
-        {
-          id: "henaojara|onair", type: "Henaojara", name: "On Air"
-        },
-        {
-          id: "tioanime|onair", type: "TioAnime", name: "On Air"
-        },
-        {
-          type: "series",
-          id: "calendar-videos",
-          extra: [
-            {
-              name: "calendarVideosIds",
-              isRequired: true,
-              optionsLimit: 15
-            }
-          ],
-          extraSupported: [
-            "calendarVideosIds"
-          ],
-          extraRequired: [
-            "calendarVideosIds"
-          ],
-          name: "Calendar videos"
+          "id": "lacartoons",
+          "type": "series",
+          "name": "LACartoons",
+          "extra": [{ "name": "search", "isRequired": false }]
         }
       ],
       "resources": [
@@ -202,32 +37,13 @@ function ReadManifest() {
         "catalog"
       ],
       "types": [
-        "movie",
         "series",
-        "anime",
-        "LACartoons"
+        "movie"
       ],
       "idPrefixes": [
-        "tt",
-        "animeflv:",
-        "animeav1:",
-        "henaojara:",
-        "tioanime:",
         "lacartoons:",
-        "tmdb:",
-        "anilist:",
-        "kitsu:",
-        "mal:",
-        "anidb:"
-      ],
-      "stremioAddonsConfig": {
-        "issuer": "https://stremio-addons.net",
-        "signature": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..0XN39hJS4zjNV5ES2brUeQ.sjRgcAHGPIUA0GXXbZI2BZLuKUOiT3jfI8ALp-QlUcWNuW_9qcVjARUxKCE6ncTE1rdK9yCma3IlgdCbI8-3ZV1E5WsKdS3LncHDeqlXThTZ9V7Znc1rATu7kJE_NDxE.Y8gIKpiHqAVypGGOvEXVqw"
-      },
-      "behaviorHints": {
-        "newEpisodeNotifications": true
-      }/*,
-      "behaviorHints": { "configurable": true }*/
+        "tt"
+      ]
     }
     return manifest;
   })
@@ -235,51 +51,12 @@ function ReadManifest() {
 
 app.get("/manifest.json", (_req, res) => {
   ReadManifest().then((manif) => {
-    //manif.behaviorHints.configurationRequired = true
     res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
     res.json(manif);
   }).catch((err) => {
     res.status(500).statusMessage("Error reading file: " + err);
   })
 })
-
-app.get("/:config/manifest.json", (_req, res) => {
-  ReadManifest().then((manif) => {
-    //console.log("Params:", decodeURIComponent(req.params[0]))
-    res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-    res.json(manif);
-  }).catch((err) => {
-    res.status(500).statusMessage("Error reading file: " + err);
-  })
-})
-
-/*app.get("/configure", (req, res) => {
-  ReadManifest().then((manif) => {
-    let base_url = req.host;
-    res.render('config', {
-      logged_in: false,
-      base_url: base_url,
-      manifest: manif
-    })
-  }).catch((err) => {
-    res.status(500).statusMessage("Error reading file: " + err);
-  })
-})
-//WIP
-app.get("/:config/configure", (req, res) => {
-  ReadManifest().then((manif) => {
-    let base_url = req.host;
-    res.render('config', {
-      logged_in: true,
-      config: req.params.config,
-      user: req.params.config,
-      base_url: base_url,
-      manifest: manif
-    })
-  }).catch((err) => {
-    res.status(500).statusMessage("Error reading file: " + err);
-  })
-})*/
 
 const streams = require("./routes/streams");
 app.use(streams);
@@ -291,25 +68,10 @@ const catalog = require("./routes/catalog");
 app.use(catalog);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`\x1b[32manimeflv-stremio-addon is listening on port ${process.env.PORT || 3000}\x1b[39m`)
-  if(process.argv.includes('--launch')){
-    const OSC = '\u001B]';
-    const BEL = '\u0007';
-    const url = `${OSC}8;;stremio://127.0.0.1:${process.env.PORT || 3000}/manifest.json${BEL}Open this link to install the running addon on the Stremio app${OSC}8;;${BEL}`
-    console.log(url)
-  } else if(process.argv.includes('--webLaunch')){
-    const url = `http://127.0.0.1:${process.env.PORT || 3000}/manifest.json`
-    console.log('Open the following for a developement web Stremio session:', `https://staging.strem.io#?addonOpen=${encodeURIComponent(url)}`)
-  }
-  const animeFLVAPI = require('./routes/animeFLV.js')
-  const animeAV1API = require('./routes/animeav1.js')
-  const henaojaraAPI = require('./routes/henaojara.js')
-  const tioanimeAPI = require('./routes/tioanime.js')
+  console.log(`\x1b[32mLACartoons Addon is listening on port ${process.env.PORT || 3000}\x1b[39m`)
+  
   const lacartoonsAPI = require('./routes/lacartoons.js')
-  let imports = [animeFLVAPI, animeAV1API, tioanimeAPI, henaojaraAPI, lacartoonsAPI]
-  imports.forEach((api) => {
-    api.UpdateAiringAnimeFile().then(() => {
-      setInterval(api.UpdateAiringAnimeFile.bind(api), 86400000); //Update every 24h
-    })
+  lacartoonsAPI.UpdateAiringAnimeFile().then(() => {
+    setInterval(lacartoonsAPI.UpdateAiringAnimeFile.bind(lacartoonsAPI), 86400000); //Update every 24h
   })
 });
--- routes/meta.js
+++ routes/meta.js
@@ -1,219 +1,26 @@
 const express = require("express")
 const metas = express.Router()
-
-require('dotenv').config()//process.env.var
-
-const Metadata = require('./metadata_copy.js')
-const relationsAPI = require('./relations.js')
-const animeFLVAPI = require('./animeFLV.js')
-const animeAV1API = require('./animeav1.js')
-const henaojaraAPI = require('./henaojara.js')
-const tioanimeAPI = require('./tioanime.js')
 const lacartoonsAPI = require('./lacartoons.js')
-const fuzzysort = require('fuzzysort')
 
-/**
- * Tipical express middleware callback.
- * @callback subRequestMiddleware
- * @param req - Request sent to our router, containing all relevant info
- * @param res - Our response
- * @param {function} [next] - The next middleware function in the chain, should end the response at some point
- */
-/** 
- * Handles requests to /stream whether they contain extra parameters (see {@link HandleLongSubRequest} for details on this) or just the type and videoID.
- * @param req - Request sent to our router, containing all relevant info
- * @param res - Our response, note we use next() just in case we need to add middleware, but the response is handled by sending an empty stream Object.
- * @param {subRequestMiddleware} [next] - The next middleware function in the chain, can be empty because we already responded with this middleware
- */
 function HandleMetaRequest(req, res, next) {
-  console.log(`\x1b[96mEntered HandleMetaRequest with\x1b[39m ${req.originalUrl}`)
+  console.log(`\x1b[96mMeta Request:\x1b[39m ${req.originalUrl}`)
   const idDetails = req.params.videoId.split(':')
-  const videoID = idDetails[0] //We only want the first part of the videoID, which is the IMDB ID, the rest would be the season and episode
-  if (videoID?.startsWith("animeflv")) {
-    const ID = idDetails[1] //We want the second part of the videoID, which is the kitsu ID
-    let episode = idDetails[2] //undefined if we don't get an episode number in the query, which is fine
-    console.log(`\x1b[33mGot a ${req.params.type} with ${videoID} ID:\x1b[39m ${ID}`)
-    animeFLVAPI.GetAnimeBySlug(ID).then((animeMeta) => {
-      console.log('\x1b[36mGot AnimeFLV metadata for:\x1b[39m', animeMeta.name)
+  const prefix = idDetails[0]
+  const slug = idDetails[1]
+
+  if (prefix === "lacartoons") {
+    lacartoonsAPI.GetShowBySlug(slug).then((animeMeta) => {
       res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-      res.json({ meta: animeMeta, message: "Got AnimeFLV metadata!" })
-      next()
+      res.json({ meta: animeMeta });
     }).catch((err) => {
-      console.error('\x1b[31mFailed on AnimeFLV slug search because:\x1b[39m ' + err)
-      if (!res.headersSent) {
-        res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-        res.json({ meta: {}, message: "Failed getting AnimeFLV info" });
-        next()
-      }
-    })
-  } else if (videoID?.startsWith("animeav1")){
-    const ID = idDetails[1] //We want the second part of the videoID, which is the kitsu ID
-    let episode = idDetails[2] //undefined if we don't get an episode number in the query, which is fine
-    console.log(`\x1b[33mGot a ${req.params.type} with ${videoID} ID:\x1b[39m ${ID}`)
-    animeAV1API.GetAnimeBySlug(ID).then((animeMeta) => {
-      console.log('\x1b[36mGot AnimeAV1 metadata for:\x1b[39m', animeMeta.name)
-      res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-      res.json({ meta: animeMeta, message: "Got AnimeAV1 metadata!" })
-      next()
-    }).catch((err) => {
-      console.error('\x1b[31mFailed on AnimeAV1 slug search because:\x1b[39m ' + err)
-      if (!res.headersSent) {
-        res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-        res.json({ meta: {}, message: "Failed getting AnimeAV1 info" });
-        next()
-      }
-    })
-  } else if (videoID?.startsWith("henaojara")){
-    const ID = idDetails[1] //We want the second part of the videoID, which is the kitsu ID
-    let episode = idDetails[2] //undefined if we don't get an episode number in the query, which is fine
-    console.log(`\x1b[33mGot a ${req.params.type} with ${videoID} ID:\x1b[39m ${ID}`)
-    henaojaraAPI.GetAnimeBySlug(ID).then((animeMeta) => {
-      console.log('\x1b[36mGot Henaojara metadata for:\x1b[39m', animeMeta.name)
-      res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-      res.json({ meta: animeMeta, message: "Got Henaojara metadata!" })
-      next()
-    }).catch((err) => {
-      console.error('\x1b[31mFailed on Henaojara slug search because:\x1b[39m ' + err)
-      if (!res.headersSent) {
-        res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-        res.json({ meta: {}, message: "Failed getting Henaojara info" });
-        next()
-      }
-    })
-  } else if (videoID?.startsWith("tioanime")){
-    const ID = idDetails[1] //We want the second part of the videoID, which is the kitsu ID
-    let episode = idDetails[2] //undefined if we don't get an episode number in the query, which is fine
-    console.log(`\x1b[33mGot a ${req.params.type} with ${videoID} ID:\x1b[39m ${ID}`)
-    tioanimeAPI.GetAnimeBySlug(ID).then((animeMeta) => {
-      console.log('\x1b[36mGot TioAnime metadata for:\x1b[39m', animeMeta.name)
-      res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-      res.json({ meta: animeMeta, message: "Got TioAnime metadata!" })
-      next()
-    }).catch((err) => {
-      console.error('\x1b[31mFailed on TioAnime slug search because:\x1b[39m ' + err)
-      if (!res.headersSent) {
-        res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-        res.json({ meta: {}, message: "Failed getting TioAnime info" });
-        next()
-      }
-    })
-  } else if (videoID?.startsWith("lacartoons")) {
-    const ID = idDetails[1]
-    console.log(`\x1b[33mGot a ${req.params.type} with ${videoID} ID:\x1b[39m ${ID}`)
-    lacartoonsAPI.GetShowBySlug(ID).then((animeMeta) => {
-      console.log('\x1b[36mGot LACartoons metadata for:\x1b[39m', animeMeta.name)
-      res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-      res.json({ meta: animeMeta, message: "Got LACartoons metadata!" })
-      next()
-    }).catch((err) => {
-      console.error('\x1b[31mFailed on LACartoons slug search because:\x1b[39m ' + err)
-      if (!res.headersSent) {
-        res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-        res.json({ meta: {}, message: "Failed getting LACartoons info" });
-        next()
-      }
+      console.error('\x1b[31mMeta error:\x1b[39m ' + err)
+      res.json({ meta: {} });
     })
   } else {
-    let episode, season, animeIMDBIDPromise
-    if (videoID?.startsWith("tt")) { //If we got an IMDB ID/TMDB ID
-      const ID = videoID //We want the IMDB ID as is
-      season = idDetails[1] //undefined if we don't get a season number in the query, which is fine
-      episode = idDetails[2] //undefined if we don't get an episode number in the query, which is fine
-      console.log(`\x1b[33mGot a ${req.params.type} with IMDB ID:\x1b[39m ${ID}`)
-      animeIMDBIDPromise = Promise.resolve(ID)
-    } else if (videoID?.startsWith("tmdb")) {
-      const ID = idDetails[1] //We want the second part of the videoID, which is the kitsu ID
-      season = idDetails[2] //undefined if we don't get a season number in the query, which is fine
-      episode = idDetails[3] //undefined if we don't get an episode number in the query, which is fine
-      console.log(`\x1b[33mGot a ${req.params.type} with TMDB ID:\x1b[39m ${ID}`)
-      animeIMDBIDPromise = Metadata.GetIMDBIDFromTMDBID(ID, req.params.type)
-    } else if (videoID.match(/^(?:kitsu|mal|anidb|anilist)$/)) { //If we got a kitsu, mal, anilist or anidb ID
-      const ID = idDetails[1] //We want the second part of the videoID, which is the kitsu ID
-      episode = idDetails[2] //undefined if we don't get an episode number in the query, which is fine
-      console.log(`\x1b[33mGot a ${req.params.type} with ${videoID} ID:\x1b[39m ${ID}`)
-      animeIMDBIDPromise = relationsAPI.GetIMDBIDFromANIMEID(videoID, ID)
-    } else {
-      if (!res.headersSent) {
-        res.header('Cache-Control', "max-age=31557600, stale-while-revalidate=31557600, stale-if-error=31557600")
-        res.json({ meta: {}, message: "Wrong ID format, check manifest for errors" }); next()
-      }
-    }
-
-    console.log('Extra parameters:', res.locals.extraParams)
-    animeIMDBIDPromise.then((imdbID) => {
-      if (!imdbID || imdbID === "null") throw Error("No IMDB ID")
-      console.log(`\x1b[33mGetting TMDB metadata for IMDB ID:\x1b[39m`, imdbID)
-      return Metadata.GetTMDBMeta(imdbID).then((TMDBmeta) => {
-        console.log('\x1b[36mGot TMDB metadata:\x1b[39m', TMDBmeta.shortPrint())
-        return TMDBmeta
-      }).catch((reason) => {
-        console.error("\x1b[31mDidn't get TMDB metadata because:\x1b[39m " + reason + ", \x1b[33mtrying Cinemeta...\x1b[39m")
-        return Metadata.GetCinemetaMeta(imdbID, req.params.type).then((Cinemeta) => {
-          console.log('\x1b[36mGot Cinemeta metadata:\x1b[39m', Cinemeta.shortPrint())
-          return Cinemeta
-        })
-      }).catch((err) => { //only catches error from TMDB or Cinemeta API calls, which we want
-        console.error('\x1b[31mFailed on metadata:\x1b[39m ' + err)
-        if (!res.headersSent) {
-          res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-          res.json({ meta: {}, message: "Failed getting media info" })
-          next()
-        }
-        throw err //We throw the error so we can catch it later
-      })
-    }).then((metadata) => {
-      const searchTerm = ((season) && (parseInt(season) !== 1)) ? `${metadata.title} ${season}` : metadata.title
-      animeFLVAPI.SearchAnimeFLV(searchTerm).then((animeFLVitem) => {
-        const result = fuzzysort.go(searchTerm, animeFLVitem, {key: 'title', limit: 1, all: true})[0]?.obj || animeFLVitem[0];
-        console.log('\x1b[36mGot AnimeFLV entry:\x1b[39m', result.title)
-        return animeFLVAPI.GetAnimeBySlug(result.slug).then((animeMeta) => {
-          console.log('\x1b[36mGot AnimeFLV metadata for:\x1b[39m', animeMeta.name)
-          res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-          res.json({ meta: animeMeta, message: "Got AnimeFLV metadata!" });
-          next()
-        })
-      }).catch((err) => {
-        if (err.message == "No search results!" && !res.headersSent) {
-          res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-          res.json({ meta: {}, message: "No search results!" })
-          next()
-        }
-        console.error('\x1b[31mFailed on animeFLV search because:\x1b[39m ' + err)
-        if (!res.headersSent) {
-          res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-          res.json({ meta: {}, message: "Failed getting animeFLV info" });
-          next()
-        }
-      })
-    }).catch((err) => {
-      console.error('\x1b[31mFailed on metadata search because:\x1b[39m ' + err)
-      if (!res.headersSent && err.message !== "Got an AnimeFLV ID/slug") {
-        res.header('Cache-Control', "max-age=86400, stale-while-revalidate=86400, stale-if-error=259200")
-        res.json({ meta: {}, message: "Failed getting media info" });
-        next()
-      }
-    })
+    res.json({ meta: {} });
   }
 }
-/** 
- * Parses the extra config parameter we can get when the addon is configured
- * @param req - Request sent to our router, containing all relevant info
- * @param res - Our response, note we use next() just in case we need to add middleware
- * @param {subRequestMiddleware} [next] - The next middleware function in the chain
- */
-function ParseConfig(req, res, next) {
-  console.log(`\x1b[96mEntered ParseConfig with\x1b[39m ${req.originalUrl}`)
-  res.locals.config = new URLSearchParams(decodeURIComponent(req.params.config))
-  console.log('Config parameters:', res.locals.config)
-  next()
-}
-//Configured requests
-metas.get("/:config/meta/:type/:videoId.json", ParseConfig, HandleMetaRequest)
-//Unconfigured requests
-metas.get("/meta/:type/:videoId.json", HandleMetaRequest)
 
-// Ensure LACartoons is handled for meta requests
-metas.get("/meta/LACartoons/:videoId.json", HandleMetaRequest)
-metas.get("/:config/meta/LACartoons/:videoId.json", ParseConfig, HandleMetaRequest)
+metas.get("/meta/:type/:videoId.json", HandleMetaRequest)
 
 module.exports = metas;
