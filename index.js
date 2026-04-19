6321f7ea67b9ca6e7b773b2af2779aafc2efc84d
alcidesmaykel154 <alcidesmaykel154@gmail.com>
2026-04-19 15:43:13 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ const manifest = {
   "description": "Addon exclusivo para ver series clásicas de LACartoons en Stremio.",
   "catalogs": [
     {
-      "id": "lacartoons",
-      "type": "series",
+      "id": "lacartoons_cat",
+      "type": "LACartoons",
       "name": "LACartoons",
       "extra": [{ "name": "search", "isRequired": false }]
     }
   ],
   "resources": ["stream", "meta", "catalog"],
-  "types": ["series", "movie"],
+  "types": ["LACartoons", "series", "movie"],
   "idPrefixes": ["lacartoons:", "tt"]
 };
 
@@ -35,7 +35,6 @@ app.get("/manifest.json", (_req, res) => {
   res.json(manifest);
 })
 
-// Support for Vercel / GitHub
 app.get("/", (_req, res) => {
   res.redirect("/manifest.json");
 })
@@ -53,6 +52,5 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(`\x1b[32mLACartoons Addon is listening on port ${process.env.PORT || 3000}\x1b[39m`)
   
   const lacartoonsAPI = require('./routes/lacartoons.js')
-  // Initial titles cache
   lacartoonsAPI.UpdateAiringAnimeFile().catch(err => console.error("Initial cache failed:", err));
 });
